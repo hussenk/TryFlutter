757db7c36c5a034463fb92d3b1d4bba235c0b751
hussenk <hussen199710@yahoo.com>
2021-12-29 18:39:51 +0200

    add new container

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,34 +13,80 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          // verticalDirection: VerticalDirection.down,
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundColor: Colors.white,
-              backgroundImage:
-                  NetworkImage('https://random.imagecdn.app/500/150'),
-            ),
-            Text(
-              'Hussen Alkunyali',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontFamily: 'Pushster',
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'Backend Developer'.toUpperCase(),
-              style: TextStyle(
-                fontSize: 15, color: Colors.white70,
-                // fontFamily: 'Pushster'
-                letterSpacing: 2.5
+          child: Column(
+            // verticalDirection: VerticalDirection.down,
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.white,
+                backgroundImage:
+                    NetworkImage('https://random.imagecdn.app/500/150'),
               ),
-            ),
-          ],
-        )),
+              Text(
+                'Hussen Alkunyali',
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontFamily: 'Pushster',
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Backend Developer'.toUpperCase(),
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white70,
+                    // fontFamily: 'Pushster'
+                    letterSpacing: 1.5),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('+218 92 2354234',
+                        style: TextStyle(
+                            fontFamily: 'Pushster',
+                            fontSize: 20,
+                            color: Colors.teal))
+                  ],
+                ),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'hussenalkunyali@hotmail.com',
+                      style: TextStyle(
+                          fontFamily: 'Pushster',
+                          color: Colors.teal,
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
