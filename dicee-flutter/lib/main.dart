b5486e31fffed656c52ac94d2a7f9d28d4af02a6
hussenk <hussen199710@yahoo.com>
2022-01-05 19:44:31 +0200

    Add stless

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -2,17 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-    MaterialApp(
+   MpApp()
+  );
+}
+
+
+class MpApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue.shade400,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blue.shade200,
         ),
         body: DicePage(),
       ),
-    ),
-  );
+    );
+  }
 }
 
 class DicePage extends StatelessWidget {
